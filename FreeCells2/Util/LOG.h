45d6b2ee841c686800c7ac006eae6288304ad110
alan.lin <alan.lin@justalk.com>
2020-10-08 17:35:28 +0800

    refactor log.h

--- FreeCells2/Util/LOG.h
+++ FreeCells2/Util/LOG.h
@@ -18,22 +18,19 @@
 #define LOG_DELEGATE_LEVEL    0
 
 #define LOG_MODOLE(TAG, LOG, ...)                                                                               \
-            int flag = LOG_LEVEL & 1 << LOG_MODEL_LEVEL;                                                        \
-            if (flag >> LOG_MODEL_LEVEL == 1)                                                                   \
+            if ((LOG_LEVEL & 1 << LOG_MODEL_LEVEL) >> LOG_MODEL_LEVEL == 1)                                     \
             {                                                                                                   \
                 NSLog((@"[%s Line %d] [" TAG "] " LOG ), __FILE_NAME__, __LINE__, ##__VA_ARGS__);               \
             }
 
 #define LOG_UI(TAG, LOG, ...)                                                                                   \
-            int flag = LOG_LEVEL & 1 << LOG_UI_LEVEL;                                                           \
-            if (flag >> LOG_UI_LEVEL == 1)                                                                      \
+            if ((LOG_LEVEL & 1 << LOG_UI_LEVEL) >> LOG_UI_LEVEL == 1)                                           \
             {                                                                                                   \
                 NSLog((@"[%s Line %d] [" TAG "] " LOG ), __FILE_NAME__, __LINE__, ##__VA_ARGS__);               \
             }
 
 #define LOG_DELEGATE(TAG, LOG, ...)                                                                             \
-            int flag = LOG_LEVEL & 1 << LOG_DELEGATE_LEVEL;                                                     \
-            if (flag >> LOG_DELEGATE_LEVEL == 1)                                                                \
+            if ((LOG_LEVEL & 1 << LOG_DELEGATE_LEVEL) >> LOG_DELEGATE_LEVEL == 1)                               \
             {                                                                                                   \
                 NSLog((@"[%s Line %d] [" TAG "] " LOG ), __FILE_NAME__, __LINE__, ##__VA_ARGS__);               \
             }
